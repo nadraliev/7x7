1bcacdb54880b574c29d7da87ffc789fa5bd0ec1
Andrew Nadraliev <andrew.nadraliev@hotmail.com>
2016-09-17 18:10:36 +0300

    added some gameplay

---
+++ 7x7/Game.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7x7
+{
+    public class Game
+    {
+        public string[] squares;
+        public readonly string[] colors = new string[3]
+        {
+            "#FF0000",
+            "#00FF00",
+            "#0000FF"
+        };
+
+
+
+        public const int MAX_COUNT = 49;
+
+
+        
+        public Game()
+        {
+            squares = new string[MAX_COUNT];
+        } 
+
+        public void GenerateNewSquares(int count)
+        {
+            Random random = new Random();
+            int generated = 0;
+            int place = 0;
+            int color = 0;
+            while (generated < count)
+            {
+                place = random.Next(0, MAX_COUNT);
+                color = random.Next(0, colors.Length);
+                if (squares[place] == null)
+                {
+                    squares[place] = colors[color];
+                    List<int> forDelete = findRow(place);
+                    if (forDelete.Count != 0)
+                        deleteSquares(forDelete);
+                    generated++;
+                }
+            }
+        }
+
+        public void findAvailableSquares(int square, ref List<int> result)
+        {
+            
+            int[] offsets = new int[4] { -7, 1, 7, -1 };
+            for (int i = 0; i < offsets.Length; i++)
+            {
+                if (square + offsets[i] >= 0 && square + offsets[i] < 49 &&squares[square + offsets[i]] == null && !result.Contains(square + offsets[i]))
+                {
+                    result.Add(square + offsets[i]);
+                    findAvailableSquares(square + offsets[i], ref result);
+                }
+
+            }
+        }
+
+        public List<int> findNotAvailableSquares(int square)
+        {
+            List<int> result = new List<int>();
+            List<int> availableSquares = new List<int>();
+            findAvailableSquares(square, ref availableSquares);
+            for (int i = 0; i < MAX_COUNT; i++)
+                if (!availableSquares.Contains(i))
+                    result.Add(i);
+            return result;
+        }
+
+        public void makeMove(int from, int to)
+        {
+            if (squares[to] == null)
+            {
+                //move squares
+                squares[to] = squares[from];
+                squares[from] = null;
+
+                //check for rows
+                List<int> forDelete = findRow(to);
+                if (forDelete.Count != 0)
+                    deleteSquares(forDelete);
+                else GenerateNewSquares(3);
+            }
+        }
+
+        public List<int> findRow(int square)    //returns numbers of squares that needs to be deleted
+        {
+
+            //check for columns
+            List<int> column = new List<int>();
+            column.Add(square);
+            int cursor = square;
+            List<int> offsets = new List<int>() { 7, -7};
+            for (int i = 0; i < offsets.Count; i++)
+            {
+                while (cursor + offsets[i] >= 0 && cursor + offsets[i] < 49 && squares[cursor + offsets[i]] == squares[square])
+                {
+                    cursor = cursor + offsets[i];
+                    column.Add(cursor);
+                }
+
+                cursor = square;
+            }
+            
+            //check for rows
+            List<int> row = new List<int>();
+            row.Add(square);
+            cursor = square;
+            offsets = new List<int>() { 1, -1};
+            for (int i = 0; i < offsets.Count; i++)
+            {
+                while (cursor + offsets[i] >= 0 && cursor + offsets[i] < 49 && squares[cursor + offsets[i]] == squares[square])
+                {
+                    cursor = cursor + offsets[i];
+                    row.Add(cursor);
+                }
+
+                cursor = square;
+            }
+
+            //check for diagonals
+            List<int> diagonal = new List<int>();
+            diagonal.Add(square);
+            cursor = square;
+            offsets = new List<int>() {8, -8, 6, -6 };
+            for (int i = 0; i < offsets.Count; i++)
+            {
+                while (cursor + offsets[i] >= 0 && cursor + offsets[i] < 49 && squares[cursor + offsets[i]] == squares[square])
+                {
+                    cursor = cursor + offsets[i];
+                    diagonal.Add(cursor);
+                }
+
+                cursor = square;
+            }
+
+            //add to result columns,rows,diagonals more than four squares
+            List<int> result = new List<int>();
+            if (column.Count >= 4) result.AddRange(column);
+            if (row.Count >= 4) result.AddRange(row);
+            if (diagonal.Count >= 4) result.AddRange(diagonal);
+
+            return result;
+        }
+
+        private void deleteSquares(List<int> forDelete)
+        {
+            for (int i = 0; i < forDelete.Count; i++)
+            {
+                squares[forDelete[i]] = null;
+            }
+        }
+
+        
+    }
+}
--- 7x7/MainWindow.xaml.cs
+++ 7x7/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace _7x7
 {
     /// <summary>
@@ -20,9 +21,84 @@ namespace _7x7
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Game newGame;
+
+        Button selectedButton;  //currently selected button
+
         public MainWindow()
         {
             InitializeComponent();
+            newGame = new Game();
         }
+
+
+        private void WrapPanel_Click(object sender, RoutedEventArgs e)
+        {
+            try {
+                Button clickedButton = (Button)e.OriginalSource;
+                if (selectedButton == null)
+                {
+                    if (newGame.squares[int.Parse(clickedButton.Tag.ToString())] != null)
+                    {
+                        selectedButton = clickedButton;
+
+                        List<int> notAvailableSquares = newGame.findNotAvailableSquares(int.Parse(selectedButton.Tag.ToString()));
+                        for (int i = 0; i < notAvailableSquares.Count; i++)
+                        {
+                            
+                            ((Button)this.FindName("button" + i)).Content = "{StaticResource MyImage}";
+                        }
+
+                        clickedButton.Opacity = 0.5;
+                    }
+                } else
+                {
+                    if (selectedButton == clickedButton)
+                    {
+                        clickedButton.Opacity = 1;
+                        selectedButton = null;
+                    } else
+                    {
+                        newGame.makeMove(int.Parse(selectedButton.Tag.ToString()), int.Parse(clickedButton.Tag.ToString()));
+                        clickedButton.Opacity = 1;
+                        selectedButton.Opacity = 1;
+                        selectedButton = null;
+                        RefreshField();
+                    }
+                }
+            } catch (InvalidCastException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            UIElementCollection buttons = gameField.Children;
+            for (int i = 0; i < buttons.Count; i++)
+                ((Button)buttons[i]).Tag = i.ToString();  //set tags for buttons here because i'm lazy to do it manually in XAML
+
+
+            newGame.GenerateNewSquares(3);
+            RefreshField();
+        }
+
+        private void RefreshField()
+        {
+            for (int i = 0; i < Game.MAX_COUNT; i++)
+            {
+                string color = newGame.squares[i];
+                if (color != null)
+                {
+                    ((Button)this.FindName("button" + i)).Background = (SolidColorBrush)new BrushConverter().ConvertFromString(color);
+                } else
+                {
+                    ((Button)this.FindName("button" + i)).Background = (SolidColorBrush)new BrushConverter().ConvertFromString("#FFDDDDDD");
+                }
+            }
+        }
+
+        
     }
 }
